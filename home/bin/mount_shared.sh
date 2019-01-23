a7b3c13aa5b779fd7ce4712dec2024dfe9d081f3
dev <dev@debian>
2019-01-23 11:05:03 -0500

    Update mount_shared

--- home/bin/mount_shared.sh
+++ home/bin/mount_shared.sh
@@ -3,6 +3,9 @@ case $CNAME in
     "icemilk")
 	mount -t smbfs //gcs6@sherbert.partners.org/shared $HOME/shared
 	;;
+    "debian")
+	sudo mount -t cifs -o user=gcs6,uid=dev //sherbert.partners.org/shared $HOME/shared
+	;;
     *)
 	sudo mount -t cifs -o user=gcs6,uid=gcs6 //sherbert.partners.org/shared $HOME/shared
 	;;
