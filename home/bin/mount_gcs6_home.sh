a6630b9b6c54099587614faf8a83f7b528b6670f
Gregory C. Sharp <gregsharp.geo@yahoo.com>
2020-09-28 14:13:41 -0400

    Update gcs_home target

--- home/bin/mount_gcs6_home.sh
+++ home/bin/mount_gcs6_home.sh
@@ -3,9 +3,9 @@
 CNAME=`uname -n`
 case $CNAME in
     "malus")
-	sudo mount -t cifs -o user=gcs6,uid=gsharp //cifs2.partners.org/homedir$ /home/gsharp/gcs6_home
+	sudo mount -t cifs -o user=gcs6,uid=gsharp //cifshd.partners.org/homedir$ /home/gsharp/gcs6_home
 	;;
     *)
-	sudo mount -t cifs -o user=gcs6,uid=gcs6 //cifs2.partners.org/homedir$ /PHShome/gcs6/gcs6_home
+	sudo mount -t cifs -o user=gcs6,uid=gcs6 //cifshd.partners.org/homedir$ /PHShome/gcs6/gcs6_home
 	;;
 esac
